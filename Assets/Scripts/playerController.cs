2449136406f4cd7b6d112a22c0aeec3a4bf54faa
mshah49 <mshah49@live.com>
2016-10-03 07:46:31 -0500

    adding some stats, and stat changes

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,6 +2,47 @@
 using System.Collections;
 
 public class playerController : MonoBehaviour {
+
+	// Mo's Code
+	public double playerHealth = 5;
+	public double playerSpeed = 1.0;
+	public double playerJump = 1.0;
+	public bool playerDoubleJump = false;
+	public double playerAttack = 3; 
+	public double playerFireRate = 1;
+
+
+	public enum playerStance{
+		brawler,heavy,mobility,
+	}
+
+	playerStance currentStance = playerStance.brawler;
+
+	void updateState(){
+		if (currentStance == playerStance.brawler){
+			playerSpeed = 1.0;
+			playerJump = 1.0;
+			playerDoubleJump = false;
+			playerAttack = 3;
+			playerFireRate = 1;
+		}
+		else if (currentStance == playerStance.heavy){
+			playerSpeed = 0.5;
+			playerJump = 0.5;
+			playerDoubleJump = false;
+			playerAttack = 2; 
+			playerFireRate = 0.5;
+		}
+		else if (currentStance == playerStance.mobility){
+			playerSpeed = 1.5;
+			playerJump = 1.5;
+			playerDoubleJump = true;
+			playerAttack = 1;
+			playerFireRate = 2;
+		}
+	}
+
+	//End Mo's Code
 	//Horizontal movement
 	public float maxSpeed;
 
