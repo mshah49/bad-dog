8686cec6350dea6cb23143ee510ead0a9a86310c
mshah49 <mshah49@live.com>
2016-11-04 09:12:36 -0500

    fixed sign-posts, now easy to create new ones, simply change messsage in inspector

--- Assets/Scripts/signPostController.cs
+++ Assets/Scripts/signPostController.cs
@@ -9,35 +9,28 @@ public class signPostController : MonoBehaviour {
 	private string textFieldString = "If you are at Brawler level 2, you can destroy boulders with your punches!";
 	bool triggered =false;
 	public Text tipText; 
-	string Tip1;
-	string Tip2;
-	string Tip3;
-
+	public string Tip = "If you are at Brawler level 2, you can destroy boulders with your punches!";
 
 
 	void OnTriggerEnter2D( Collider2D other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			triggered = true;
-			print("collision");
+			tipText.text = Tip;
+
 		}
 	}
 
 	void Update() {
-		Tip1 = "If you are at Brawler level 2, you can destroy boulders with your punches!";
-		if (triggered) {
-			tipText.text = Tip1;
-		} else {
-			tipText.text = "";
-		}
+		
 	}
 
 	void OnTriggerExit2D( Collider2D other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			triggered =false;
+			tipText.text = "";
 		}
 	}
+
 }
