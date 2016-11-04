d080f7ea7588aa0bb8ea0d8177f6d5c086411762
mshah49 <mshah49@live.com>
2016-11-04 07:58:04 -0500

    tryin to set stanceStatus box to top right

--- Assets/Scripts/stanceStatusGUI.cs
+++ Assets/Scripts/stanceStatusGUI.cs
@@ -14,7 +14,11 @@ public class stanceStatusGUI : MonoBehaviour {
 	private string MobilityString;
 	private string HeavyString;
 
+
 	bool triggered =false;
+	void Start () {
+		
+	}
 
 	void OnGUI() {
 		getStanceLevel ();
@@ -23,14 +27,18 @@ public class stanceStatusGUI : MonoBehaviour {
 		HeavyString = "Heavy:Level " + heavyLevel;
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerController playerController = player.GetComponent<playerController> ();
+
+
+		GUI.skin.textField.fontSize = Screen.width/50;
+
 		if (playerController.currentStance ==playerController.playerStance.brawler) {
-			BrawlerString = GUI.TextField (new Rect (500, 25, 200, 30), BrawlerString);
+			BrawlerString = GUI.TextField (new Rect (Screen.width - 100,0,100,50), BrawlerString);
 		}
 		if (playerController.currentStance ==playerController.playerStance.mobility) {
-			MobilityString = GUI.TextField (new Rect (500, 25, 200, 30),  MobilityString);
+			MobilityString = GUI.TextField (new Rect (Screen.width - 100,0,100,50),  MobilityString);
 		}
 		if (playerController.currentStance ==playerController.playerStance.heavy) {
-			HeavyString = GUI.TextField (new Rect (500, 25, 200, 30), HeavyString);
+			HeavyString = GUI.TextField (new Rect (Screen.width - 100,0,100,50), HeavyString);
 		}
 	}
 
