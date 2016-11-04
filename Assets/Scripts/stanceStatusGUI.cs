1bd855629fd992810a4aac652bc93fc92b8b4d42
mshah49 <mshah49@live.com>
2016-11-04 07:33:29 -0500

    added text that tells players what stance they are in, and their level in that stance

---
+++ Assets/Scripts/stanceStatusGUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.IO;
+
+public class stanceStatusGUI : MonoBehaviour {
+
+	private GameObject player;
+	private int brawlerLevel;
+	private int mobilityLevel;
+	private int heavyLevel;
+	private string BrawlerString;
+	private string MobilityString;
+	private string HeavyString;
+
+	bool triggered =false;
+
+	void OnGUI() {
+		getStanceLevel ();
+		BrawlerString = "Brawler:Level " + brawlerLevel;
+		MobilityString = "Mobility:Level " + mobilityLevel;
+		HeavyString = "Heavy:Level " + heavyLevel;
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerController playerController = player.GetComponent<playerController> ();
+		if (playerController.currentStance ==playerController.playerStance.brawler) {
+			BrawlerString = GUI.TextField (new Rect (25, 25, 200, 30), BrawlerString);
+		}
+		if (playerController.currentStance ==playerController.playerStance.mobility) {
+			MobilityString = GUI.TextField (new Rect (25, 25, 200, 30),  MobilityString);
+		}
+		if (playerController.currentStance ==playerController.playerStance.heavy) {
+			HeavyString = GUI.TextField (new Rect (25, 25, 200, 30), HeavyString);
+		}
+	}
+
+	void getStanceLevel(){
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerController playerController = player.GetComponent<playerController> ();
+		brawlerLevel = playerController.brawlerLevel;
+		mobilityLevel = playerController.mobilityLevel;
+		heavyLevel = playerController.heavyLevel;
+	}
+}
