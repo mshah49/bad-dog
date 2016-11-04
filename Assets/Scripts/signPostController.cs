eee9f218a7a107b65eb2e81cd94b903e2552306d
mshah49 <mshah49@live.com>
2016-11-04 08:49:02 -0500

    fixed stance status text, and tip text placement using UI

--- Assets/Scripts/signPostController.cs
+++ Assets/Scripts/signPostController.cs
@@ -3,10 +3,17 @@ using System.Collections;
 using System;
 using System.Text;
 using System.IO;
+using UnityEngine.UI;
 
 public class signPostController : MonoBehaviour {
 	private string textFieldString = "If you are at Brawler level 2, you can destroy boulders with your punches!";
 	bool triggered =false;
+	public Text tipText; 
+	string Tip1;
+	string Tip2;
+	string Tip3;
+
+
 
 	void OnTriggerEnter2D( Collider2D other)
 	{
@@ -17,9 +24,12 @@ public class signPostController : MonoBehaviour {
 		}
 	}
 
-	void OnGUI() {
+	void Update() {
+		Tip1 = "If you are at Brawler level 2, you can destroy boulders with your punches!";
 		if (triggered) {
-			textFieldString = GUI.TextField (new Rect (25, 25, 500, 30), textFieldString);
+			tipText.text = Tip1;
+		} else {
+			tipText.text = "";
 		}
 	}
 
--- Assets/Scripts/stanceStatusGUI.cs
+++ Assets/Scripts/stanceStatusGUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System;
 using System.Text;
 using System.IO;
+using UnityEngine.UI;
 
 public class stanceStatusGUI : MonoBehaviour {
 
@@ -11,16 +12,16 @@ public class stanceStatusGUI : MonoBehaviour {
 	private int mobilityLevel;
 	private int heavyLevel;
 	private string BrawlerString;
+	private string BrawlerString2;
 	private string MobilityString;
 	private string HeavyString;
-
+	public Text stanceText;  
 
 	bool triggered =false;
 	void Start () {
-		
 	}
 
-	void OnGUI() {
+	void Update() {
 		getStanceLevel ();
 		BrawlerString = "Brawler:Level " + brawlerLevel;
 		MobilityString = "Mobility:Level " + mobilityLevel;
@@ -28,17 +29,14 @@ public class stanceStatusGUI : MonoBehaviour {
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerController playerController = player.GetComponent<playerController> ();
 
-
-		GUI.skin.textField.fontSize = Screen.width/50;
-
 		if (playerController.currentStance ==playerController.playerStance.brawler) {
-			BrawlerString = GUI.TextField (new Rect (Screen.width - 100,0,100,50), BrawlerString);
+			stanceText.text = BrawlerString;
 		}
 		if (playerController.currentStance ==playerController.playerStance.mobility) {
-			MobilityString = GUI.TextField (new Rect (Screen.width - 100,0,100,50),  MobilityString);
+			stanceText.text = MobilityString;
 		}
 		if (playerController.currentStance ==playerController.playerStance.heavy) {
-			HeavyString = GUI.TextField (new Rect (Screen.width - 100,0,100,50), HeavyString);
+			stanceText.text = HeavyString;
 		}
 	}
 
