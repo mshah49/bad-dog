89ae59d9ddec2071bbd683e7d428ddc95898b42a
mshah49 <mshah49@live.com>
2016-11-03 21:52:14 -0500

    fixed projectile death time

--- Assets/Scripts/DestroyProjectile.cs
+++ Assets/Scripts/DestroyProjectile.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
+
 
 public class DestroyProjectile : MonoBehaviour {
 	public float timeAlive;
+	public GameObject player;
 	// Use this for initialization
 	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerController playerController = player.GetComponent<playerController> ();
+		if (playerController.currentStance == playerController.playerStance.brawler) {
+			timeAlive = 0.5f;
+		}
+		else if (playerController.currentStance == playerController.playerStance.mobility) {
+			timeAlive = 5;
+		}	
+
 		Destroy (gameObject, timeAlive);
 		
 	}
